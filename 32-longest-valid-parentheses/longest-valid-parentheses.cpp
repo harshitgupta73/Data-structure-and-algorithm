b572d8f0ebf2a041103bc502820cede85a3c54d6
Harshit Gupta <officialharshit73071@gmail.com>
2025-12-29 11:54:28 +0530

    Time: 1 ms (54.87%) | Memory: 11.8 MB (50.65%) - LeetSync

---
+++ 32-longest-valid-parentheses/longest-valid-parentheses.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int longestValidParentheses(string s) {
+        stack<int> st;
+        int n=s.length();
+        st.push(-1);
+        int maxi=0;
+        for(int i=0;i<n;i++){
+            if(s[i]=='(') st.push(i);
+            else if(s[i]==')'){
+                st.pop();
+                if(st.empty()){
+                    st.push(i);
+                }
+                maxi= max(maxi,i-st.top()); 
+            }
+        }
+        return maxi;
+    }
+};
